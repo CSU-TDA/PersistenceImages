c0ec0905ae928c4ed028ba1b36b1fbc85fb5a05a
Henry Adams <henryhughadams@gmail.com>
2016-03-06 15:00:44 -0700

    Added the file minimal_example.m and the accompanying data file circleAndTorusIntervals.mat. The matlab script minimal_example.m plots the persistent images for the H1 diagrams for a noisy circle and torus, as well as the persistent vectors for the H0 diagrams for a noisy circle and torus.

---
+++ matlab_code/minimal_example.m
@@ -0,0 +1,35 @@
+%% A minimal example using the make_PIs and make_PVs functions
+close all
+clear all
+load('circleAndTorusIntervals.mat')
+% circleAndTorusIntervals_H1 is a 1x2 cell containing the H1 intervals for
+% points sampled from a noisy circle and torus.
+% circleAndTorusIntervals_H0 is a 1x2 cell containing the H0 intervals from
+% points sampled from a noisy circle and torus.
+
+bump_func_params=[0,0];
+sig=0.0001;
+res=50;
+[H1_PIs] = make_PIs(circleAndTorusIntervals_H1, res, sig, bump_func_params, 1);
+figure, imagesc(H1_PIs{1})
+title('Persistant Image for H1 diagram of points sampled from a noisy circle')
+figure, imagesc(H1_PIs{2})
+title('Persistant Image for H1 diagram of points sampled from a noisy torus')
+
+% Since all of the H0 intervals begin at time zero, we produce a
+% 1-dimensional "vector grid" on top of this persistence diagram (instead
+% of a 2-dimesional "image grid").
+[H0_PVs] = make_PVs(circleAndTorusIntervals_H0, res, sig);
+figure, imagesc(H0_PVs{1}), axis equal
+title('Persistant Vector for H0 diagram of points sampled from a noisy circle')
+figure, imagesc(H0_PVs{2}), axis equal
+title('Persistant Vector for H0 diagram of points sampled from a noisy torus')
+
+%% Convert images to vectors
+
+% Before applying machine learning techniques, it is often helpful to
+% conver a persistent image in matrix format into a vector by concatenating
+% columns. This is accomplished by the following command.
+H1_vecs = vecs_from_PIs(H1_PIs);
+
+
